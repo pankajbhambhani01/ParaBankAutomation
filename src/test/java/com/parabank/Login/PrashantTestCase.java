dedf0223a07f94defdd947a4e5380096467c3d71
PrashantBagde <j22.prashantb@gmail.com>
2022-05-01 17:52:36 +0530

    commit by prashant

---
+++ src/test/java/com/parabank/Login/PrashantTestCase.java
@@ -0,0 +1,9 @@
+package com.parabank.Login;
+
+import java.sql.SQLOutput;
+
+public class PrashantTestCase {
+    public static void main(String[] args) {
+        System.out.println("Hello Sir");
+    }
+}
