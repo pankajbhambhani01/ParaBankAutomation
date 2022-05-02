fe98a1f402d6f9828414b29bf82de48c87f66358
Abhijit Raoot <j22.abhijit@gmail.com>
2022-05-02 23:30:29 +0530

    commit by Abhijit Raoot

--- src/test/java/com/parabank/Login/AbhijitTestCase.java
+++ src/test/java/com/parabank/Login/AbhijitTestCase.java
@@ -3,6 +3,10 @@ package com.parabank.Login;
 public class AbhijitTestCase {
     public static void main(String[] args) {
         System.out.println("parabank");
+
+        System.out.println("new project");
     }
+
+
 
 }
