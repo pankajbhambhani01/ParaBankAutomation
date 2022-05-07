0f1ee81dca1ef4e426cc0f173df76493a0894782
Prashant Gujar <j22.prashantgujar@gmail.com>
2022-05-07 07:59:21 +0530

    Prashant G First Testcase

---
+++ src/test/java/com/tests/login/PrashantGujarTestCase.java
@@ -0,0 +1,14 @@
+package com.tests.login;
+
+public class PrashantGujarTestCase {
+
+    public static void main(String[] args) {
+        System.out.println("parabank");
+
+        System.out.println("new project");
+        System.out.println("para");
+    }
+
+
+
+}
