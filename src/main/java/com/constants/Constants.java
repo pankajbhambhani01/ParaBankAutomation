cea4d7161530fe1a8506ea0ce9c7a04e71170ceb
Pankaj B <pankajbhambhani01@gmail.com>
2022-05-09 09:42:33 +0530

    framework changes - browser, utilities, constants, beforeafter, listners

---
+++ src/main/java/com/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.constants;
+
+public class Constants {
+    public static String browser ="";
+    public static String env = "";
+    public static final String pathTestResources = "src/test/resources/";
+    public static final String pathMainResources = "src/main/resources/";
+    public static int passedCount = 0;
+    public static int failedCount = 0;
+}
---
+++ src/test/java/com/utilities/Listners.java
@@ -0,0 +1,41 @@
+package com.utilities;
+
+import com.constants.Constants;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class Listners implements ITestListener {
+    public void onTestStart(ITestResult iTestResult) {
+        Reporter.log("Test cases has started " + iTestResult.getTestName());
+    }
+
+    public void onTestSuccess(ITestResult iTestResult) {
+        Reporter.log("Test cases has been Passed " + iTestResult.getTestName());
+        Constants.passedCount = Constants.passedCount + 1;
+        System.out.println("Current Execution Status : Passed " + Constants.passedCount + " Failed " + Constants.failedCount);
+    }
+
+    public void onTestFailure(ITestResult iTestResult) {
+        Reporter.log("Test cases has been Failed " + iTestResult.getTestName());
+        Constants.failedCount = Constants.failedCount + 1;
+        System.out.println("Current Execution Status : Passed " + Constants.passedCount + " Failed " + Constants.failedCount);
+    }
+
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+}
