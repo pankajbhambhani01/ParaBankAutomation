854175992fbfa2d914825befc595fe4ca5be0e3c
Pankaj B <pankajbhambhani01@gmail.com>
2022-05-10 10:08:12 +0530

    changes for testcases

--- src/test/java/com/tests/login/LoginTestCases.java
+++ src/test/java/com/tests/login/LoginTestCases.java
@@ -1,6 +1,7 @@
 package com.tests.login;
 
 import com.constants.Constants;
+import com.pages.LoginPage;
 import com.tests.utilities.PrePost;
 import com.utility.Browser;
 import com.utility.FileReading;
@@ -8,11 +9,18 @@ import org.testng.Reporter;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.Map;
+
 public class LoginTestCases extends PrePost {
 
-    @Test
+    @Test(testName = "Title of your test cases from TMT")
     public void TC001_ValidateAllLablesOnLoginScreen(){
-        Reporter.log(browser.getDriver().toString()) ;
-        Reporter.log("Running my first test");
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage = new LoginPage(browser.getDriver());
+        loginPage.validateCustomerLabel(testData.get("customer_login"));
+        loginPage.validateUserNameLabel(testData.get("username"));
+        // assert and verify(soft assert) -
+        // assert - if it fails it stops the test there
+        // verify - it continues the test
     }
 }
