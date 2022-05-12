996b85ad5db846a3a96c363155939e6c670820cd
Abhijit Raoot <j22.abhijit@gmail.com>
2022-05-12 08:09:34 +0530

    commit By Abhijit

---
+++ src/test/java/com/tests/login/ForgotPasswordTestCase.java
@@ -0,0 +1,50 @@
+package com.tests.login;
+
+import org.testng.annotations.Test;
+
+public class ForgotPasswordTestCase {
+    //Abhijit raoot
+
+    @Test
+    public void TC001_validateForgotPasswordLinkNavigation(){
+        //click on Forgot Password Info? Link
+        //validate Customer Page appered.
+    }
+
+    @Test
+    public void TC002_entercorrectInfoInFieldForgotPassword(){
+        //click on Forgot Password Info? Link
+        //enter All Field with correct information
+        //click on FindMylogininfoBtn
+        //validate message
+
+    }
+
+    @Test
+    public void TC003_enterWrongInfoInFieldForgotPassword(){
+        //click on Forgot Password Info? Link
+        //enter All Field with wrong information
+        //click on FindMylogininfoBtn
+        //validate message
+
+    }
+
+    @Test
+    public void TC004_enterSomeFieldBlankINForgotPassword(){
+        //click on Forgot Password Info? Link
+        //enter name city phonenumber Field with wrong information
+        //click on FindMylogininfoBtn
+        //validate message
+
+    }
+
+
+    @Test
+    public void TC005_KeepAllFieldBlankINForgotPassword(){
+        //click on Forgot Password Info? Link
+        //click on FindMylogininfoBtn
+        //validate message
+
+    }
+
+}
--- src/test/java/com/tests/login/LoginTestCases.java
+++ src/test/java/com/tests/login/LoginTestCases.java
@@ -2,6 +2,7 @@ package com.tests.login;
 
 import com.constants.Constants;
 import com.pages.LoginPage;
+import com.pages.RegistrationPage;
 import com.tests.utilities.PrePost;
 import com.utility.Browser;
 import com.utility.FileReading;
@@ -26,11 +27,11 @@ public class LoginTestCases extends PrePost {
         loginPage.validateServicesLink(testData.get("services"));
         loginPage.validateProductLink(testData.get("products"));
         loginPage.validateLocationLink(testData.get("locations"));
-        loginPage.validateLocationLink(testData.get("admin_Page"));
+        loginPage.validateAdminPageLink(testData.get("admin_Page"));
         loginPage.validateCustomerLabel(testData.get("customer_login"));
         loginPage.validateUserNameLabel(testData.get("username"));
-        loginPage.validatePasswordLabel(testData.get("Password"));
-        loginPage.validateLoginButton(testData.get("log In"));
+        loginPage.validatePasswordLabel(testData.get("password"));
+        loginPage.validateLoginButton(testData.get("log_in"));  //some problem
         loginPage.validateForgotPasswordLink(testData.get("forgot_login_info_?"));
         loginPage.validateRegisterLink(testData.get("register"));
 
@@ -46,7 +47,16 @@ public class LoginTestCases extends PrePost {
         // enter password
         // click login
         // validate home page
-    }
+
+
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage=new LoginPage(browser.getDriver());
+        loginPage.enterUserName("");
+        loginPage.enterPassword("");
+        loginPage.clickLoginButton();
+        loginPage.validateAccountOverViewTittle("");
+
+}
 
     @Test
     public void TC003_InvalidUsername(){
@@ -54,15 +64,127 @@ public class LoginTestCases extends PrePost {
         // enter wrong password
         // click login
         // validate error message
+
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage=new LoginPage(browser.getDriver());
+        loginPage.enterUserName("");
+        loginPage.enterPassword("");
+        loginPage.clickLoginButton();
+        loginPage.validateWrongErrorMessage("");
     }
 
     @Test
     public void TC004_InvalidPassword(){
+        //enter userName
+        //enter wrong password
+        //click login
+        //validate error message
+
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage=new LoginPage(browser.getDriver());
+        loginPage.enterUserName("");
+        loginPage.enterPassword("");
+        loginPage.clickLoginButton();
     }
 
     @Test
     public void TC005_BlankUsernamePassword(){
+
+        //click login
+        //validate error message
+
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage=new LoginPage(browser.getDriver());
+        loginPage.clickLoginButton();
+        loginPage.validateBlankErrorMessage("");
+
     }
+    @Test
+    public void TC006_validateForgotLoginInfoLink(){
+
+        //click on forgot login info?
+        //validate customer Lookup page open
+
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage=new LoginPage(browser.getDriver());
+        loginPage.clickForgotPasswordLink();
+        loginPage.validateCustomerLabel("");
+
+    }
+
+    @Test
+    public void TC007_validateRegisterLink(){
+
+        //click on registerlink
+        //validate Signing up is easy! page open
+
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage=new LoginPage(browser.getDriver());
+        loginPage.clickRegisterLink();
+      //  RegistrationPage registrationPage=new RegistrationPage(browser.getDriver());
+        //registrationPage.validateRegistrationLabel("");
+
+
+    }
+    @Test
+    public void TC008_validateAboutusLink(){
+        //click on Aboutus link
+        //validate customer ParaSoft Demo Website page open
+
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage=new LoginPage(browser.getDriver());
+        loginPage.clickAboutUslink();
+        loginPage.validateParasoftTittle("");
+    }
+
+    @Test
+    public void TC009_validateServicesLink(){
+        //click on Services link
+        //validate customer Available Bookstore SOAP services: page open
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage=new LoginPage(browser.getDriver());
+        loginPage.clickServicesLink();
+        loginPage.validateServicePageTittle("");
+
+    }
+
+    @Test
+    public void TC0010_validateProductLink(){
+        //click on Product link
+        //validate customer Parasoft  page open
+
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage=new LoginPage(browser.getDriver());
+        loginPage.clickProductLink();
+        loginPage.validateProductPageTittle("");
+
+    }
+
+    @Test
+    public void TC0011_validateLocationsLink(){
+        //click on locations link
+        //validate customer Parasoft  page open
+
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage=new LoginPage(browser.getDriver());
+        loginPage.clickLocationLink();
+        loginPage.validateLocationPageTittle("");
+
+    }
+
+    @Test
+    public void TC0012_validateAdminPageLink(){
+        //click on Adminpage link
+        //validate Administration  page open
+
+        Map<String, String> testData = FileReading.readProperties("TC001");
+        LoginPage loginPage=new LoginPage(browser.getDriver());
+        loginPage.clickAdminPageLink();
+        loginPage.validateAdminPageLink("");
+
+    }
+
+
 
 
 }
