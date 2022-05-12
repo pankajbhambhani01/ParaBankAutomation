62b71a053421fa478b0eb4ea8ad7c18226491648
Vrushali <j22.vrushali@gmail.com>
2022-05-12 08:11:11 +0530

    Test cases

---
+++ src/test/java/com/tests/login/BillPaymentTestCases.java
@@ -0,0 +1,98 @@
+package com.tests.login;
+
+import com.pages.BillPaymentServicePage;
+import com.tests.utilities.PrePost;
+import com.utility.FileReading;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+public class BillPaymentTestCases {
+    @Test
+    public void TC001_ValidateBillPayPageNevigation() {
+        //click on bill pay link
+        //validate the page
+
+    }
+
+    @Test
+    public void TC002_SucessfullPayment() {
+        //click on Bill Payment service label
+        //enter valid  Payee Name
+        //enter valid Address
+        //enter valid city
+        //enter valid State
+        //enter valid ZipCode
+        //enter valid Phone number
+        //enter valid Account number
+        //enter valid Verify account number
+        //enter valid Amount
+        //enter valid from account number
+        //click on send payment button
+        //validate the page
+    }
+        @Test
+        public void TC003_InvalidPayeeName() {
+            //enter invalid PayeeName
+            //click on send Payment button
+            //validate error message
+
+        }
+        @Test
+        public void TC004_InvalidAccountNumber() {
+            //enter invalid account number
+            //click on send send payment button
+            //validate error message
+        }
+        @Test
+        public void TC005_EmptyTextFields(){
+            //All fields are empty
+            //click on send payment button
+            //validate error message
+       }
+       @Test
+       public void TC006_InvalidVerifyAccountNumber(){
+             //enter valid acoount number
+             //enter invalid VerifyAccountNumber
+             //click on send Payment button
+             //validate error message
+       }
+       @Test
+       public void TC007_InvalidPhoneNumber(){
+             //enter all valid fileds
+             //enter invalid phone Number
+            //validate error message
+       }
+       @Test
+       public void TC008_InvalidState(){
+              //enter all valid fields
+             //enter invalid State
+            //validate error message
+       }
+       @Test
+       public void TC009_InvalidCity(){
+             //enter all valid fields
+            //enter invalid city
+           //validate error message
+       }
+       @Test
+        public void TC010_InvalidZipCode(){
+             //enter all valid fields
+            //enter invalid zipcode
+            //validate error message
+       }
+       @Test
+       public void TC011_InvalidFromAccount(){
+            //enter all valid fields
+            //enter invalid fromAccountId
+            //validate error message
+       }
+       @Test
+       public void TC012_SendPaymentButtonNotClickable(){
+            //enter all valid fields
+            //SendPayment button not clickable
+            //validate error message
+       }
+
+
+}
